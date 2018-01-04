e5144cc59de0f2a43a4fedada89da70f0564c9f4
zxl <8397328520@qq.com>
2018-01-05 00:14:08 +0800

    millet

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index">
+    <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+import searchBar from "./index/searchBar";
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  },
+  components:{
+    'search-bar': searchBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/index/searchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search-bar">
+    <div>123</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'searchBar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
